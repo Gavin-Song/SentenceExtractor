b962a9ae2f743d4010f1463a5b21ea48aab9d63b
Gavin-Song <gavin.song.bc@gmail.com>
2017-05-06 12:34:45 -0400

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,17 @@
+'use strict';
+var extract = require("../index.js");
+
+console.log("Testing extraction on a basic sentence");
+console.log("--------------------------------------");
+console.log( extract( "This is a sentence. Cool! Maybe? 3.141592 is a number." ) );
+
+console.log("");
+console.log("Testing extraction with custom dividers");
+console.log("--------------------------------------");
+console.log( extract( "This is a sentence ending in | Another ) ", false, ["|",")"] ) );
+
+console.log("");
+console.log("Testing extraction with custom abbrev.");
+console.log("--------------------------------------");
+console.log( extract( "Po. test is a tf. person.", ["po","tf"] ) );
+
